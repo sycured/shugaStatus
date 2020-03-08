684928f0fcba32635b11f706224c9d3153da905b
mkobar <mkobar@rkosecurity.com>
2020-03-07 19:14:58 -0500

    added maps

--- index.js
+++ index.js
@@ -172,7 +172,9 @@ const data = {
             "m784437563-3180dfa269b28789a8f786f6", //Place your next key here! <- app demo
             "m784438298-02f8d06be96a9c9e468770d4"], //Place your next key here! <- API GUI
     "APIs": ["m784438306-8c3c187da5a48781f6b4c6f4", //Place your next key here! <- API Google
-             "m784438301-f480bb16afc2bdcd045fc3aa"] //Place your next key here! <- API SER
+             "m784438301-f480bb16afc2bdcd045fc3aa"], //Place your next key here! <- API SER
+    "Maps": ["m784469888-bf0e77a463ae9e95f94aee39", // Map proto 1
+			"m784469890-5fce49f43d89cdc79d8545cb"]  // Map proto 2
 }
 
 document.body.addEventListener("load", ShugaStatus.init());
